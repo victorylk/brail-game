bc6bf09bed30af95b17bbb232d9ef47102bda365
quickcoco4 <quickcoco4@gmail.com>
2024-05-08 17:21:07 +0800

    服务器返回登陆成功的消息

--- 0010_Brazil/assets/Texture/Slot_fortunetiger/js/fortunetigerNetwork.js
+++ 0010_Brazil/assets/Texture/Slot_fortunetiger/js/fortunetigerNetwork.js
@@ -55,6 +55,13 @@ cc.Class({
             if (data.resultid == 1) {
                 this.mainObj.setPool(data.Obj.nGamblingWinPool);
             }
+
+            this.playerInfo.playerId = data.Obj.id;
+            this.playerInfo.playerName = data.Obj.nickname;
+            this.playerInfo.playerCoin = data.Obj.score;            
+            this.playerInfo.playerAccount = data.Obj.account;            
+            this.playerInfo.win_pool = data.Obj.nGamblingWinPool;
+            this.mainObj.slotCtrl.lblUserCoin.string = Helper.fixNum(data.Obj.score);
         });
 
         this.socket.on('lotteryResult', data => {
