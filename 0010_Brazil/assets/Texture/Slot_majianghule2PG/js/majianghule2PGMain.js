de94174255dbdacc868d548df7b77491c4f2c8c8
quickcoco4 <quickcoco4@gmail.com>
2024-07-23 15:40:05 +0800

    英文版本

--- 0010_Brazil/assets/Texture/Slot_majianghule2PG/js/majianghule2PGMain.js
+++ 0010_Brazil/assets/Texture/Slot_majianghule2PG/js/majianghule2PGMain.js
@@ -132,10 +132,20 @@ cc.Class({
         this.isTimeOut = false;
         if (GameGlobal.LANG == 'cn') {
             cc.find("Canvas/GameSplash/splash_cn").active = true
+            cc.find("Canvas/freegame_start/free_spin_won_cn").active = true
+            cc.find("Canvas/freegame_start/freature_loading_cn").active = true
+            cc.find("Canvas/freegame_start/bonus_button/start_cn").active = true
+            cc.find("Canvas/freegame_end/free_spin_won_cn").active = true
+            cc.find("Canvas/freegame_end/bonus_button/start_cn").active = true
             this.tipCnNode.active = true
         }
         else {
             cc.find("Canvas/GameSplash/splash_en").active = true
+            cc.find("Canvas/freegame_start/free_spin_won_en").active = true
+            cc.find("Canvas/freegame_start/freature_loading_en").active = true
+            cc.find("Canvas/freegame_start/bonus_button/start_en").active = true
+            cc.find("Canvas/freegame_end/free_spin_won_en").active = true
+            cc.find("Canvas/freegame_end/bonus_button/start_en").active = true
             this.tipNode.active = true;
         }
         
